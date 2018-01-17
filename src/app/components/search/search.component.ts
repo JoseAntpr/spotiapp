a24c84f29c1ca8f2d0806e2ad64d4385a858d357
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-01-17 14:09:55 +0100

    Operator map added to SpotifyService

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -10,7 +10,11 @@ import { SpotifyService } from "../../services/spotify.service";
 export class SearchComponent {
 
   constructor(public _spotify: SpotifyService) { 
-    this._spotify.getArtistas();
+    this._spotify.getArtistas()
+        .subscribe( artistas => { 
+          console.log('Información lista'); 
+          console.log( artistas );
+        });;
   }
 
 }
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class SpotifyService {
+
+  artistas: any[] = []
 
   constructor( public http: HttpClient) { 
     console.log('Servicio de spotify listo')
@@ -11,10 +14,15 @@ export class SpotifyService {
   getArtistas(){
     let url = 'https://api.spotify.com/v1/search?query=Metallica&type=artist&limit=20';
 
-    this.http.get(url)
-          .subscribe( resp => { console.log(resp); });
+    let headers = new HttpHeaders({
+      'authorization': 'Bearer BQBGRiXZOKWxeflLcfouoX-z9-4zhbJY70xgH9mMg4SPtQ5H7o1lU81YfboLFwcupeO0dUuR3stSFBGxT_I'
+    });
 
-
+    return this.http.get(url, { headers})
+                .map( (resp: any) => {
+                  this.artistas = resp.artists.items;
+                  return this.artistas;
+                })
   }
 
 }
