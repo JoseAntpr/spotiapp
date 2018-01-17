a37224b1aa682ebf4c5912b70ca43812b77f303f
José Antonio Palacios Ramírez <joseantpalaciosramirez@gmail.com>
2018-01-17 13:44:04 +0100

    Spoty Service created and added to the search component

---
+++ src/app/services/spotify.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class SpotifyService {
+
+  constructor( public http: HttpClient) { 
+    console.log('Servicio de spotify listo')
+  }
+
+  getArtistas(){
+    let url = 'https://api.spotify.com/v1/search?query=Metallica&type=artist&limit=20';
+
+    this.http.get(url)
+          .subscribe( resp => { console.log(resp); });
+
+
+  }
+
+}
